cd21e2e14af798ff85d2070c1290b500a7df52cc
sudhakarp3 <sudha2u@gmail.com>
2018-07-25 05:33:05 +0400

    unit test added page 143

---
+++ CH6-EssentialToolsTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CH6-EssentialToolsTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CH6-EssentialToolsTest")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("6c2d7d67-6049-4b91-b221-6cc279b36f56")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ CH6-EssentialToolsTest/UnitTest1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using CH6_EssentialTools.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CH6_EssentialToolsTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private IDiscountHelper getTestObject()
+        {
+            return new MinimumDiscountHelper();
+        }
+
+        [TestMethod]
+        public void Discount_Above_100()
+        {
+            // arrange
+            IDiscountHelper target = getTestObject();
+            decimal total = 200;
+            // act
+            var discountedTotal = target.ApplyDiscount(total);
+            // assert
+            Assert.AreEqual(total * 0.9M, discountedTotal);
+
+        }
+    }
+}
