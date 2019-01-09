f11bf8ff5cdc26a0cb58b9d573d21e6ef2c71ba6
sudhakarp3 <sudha2u@gmail.com>
2019-01-09 05:31:49 +0400

    page 204 category with pagination

--- SportsStore.UnitTests/UnitTest1.cs
+++ SportsStore.UnitTests/UnitTest1.cs
@@ -21,6 +21,35 @@ namespace SportsStore.UnitTests
         {
         }
         [TestMethod]
+        public void Can_Filter_Products()
+        {
+
+            // Arrange
+            // - create the mock repository
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new Product[] {
+            new Product {ProductID = 1, Name = "P1", Category = "Cat1"},
+            new Product {ProductID = 2, Name = "P2", Category = "Cat2"},
+            new Product {ProductID = 3, Name = "P3", Category = "Cat1"},
+            new Product {ProductID = 4, Name = "P4", Category = "Cat2"},
+            new Product {ProductID = 5, Name = "P5", Category = "Cat3"}
+            });
+            // Arrange - create a controller and make the page size 3 items
+            ProductController controller = new ProductController(mock.Object);
+            controller.PageSize = 3;
+
+            // Action
+            Product[] result = ((ProductsListViewModel)controller.List("Cat2", 1).Model)
+                .Products.ToArray();
+
+            // Assert
+            Assert.AreEqual(result.Length, 2);
+            Assert.IsTrue(result[0].Name == "P2" && result[0].Category == "Cat2");
+            Assert.IsTrue(result[1].Name == "P4" && result[1].Category == "Cat2");
+
+        }
+
+        [TestMethod]
         public void Can_Paginate()
         {
 
@@ -39,7 +68,7 @@ namespace SportsStore.UnitTests
 
             // Act
             IEnumerable<Product> result =
-                (IEnumerable<Product>)controller.List(2).Model;
+                (IEnumerable<Product>)controller.List(null,2).Model;
 
             // Assert
             Product[] prodArray = result.ToArray();
@@ -94,7 +123,7 @@ namespace SportsStore.UnitTests
             controller.PageSize = 3;
 
             // Act
-            ProductsListViewModel result = (ProductsListViewModel)controller.List(2).Model;
+            ProductsListViewModel result = (ProductsListViewModel)controller.List(null,2).Model;
 
             // Assert
             /*PagingInfo pageInfo = result.PagingInfo;
--- SportsStore.WebUI/App_Start/RouteConfig.cs
+++ SportsStore.WebUI/App_Start/RouteConfig.cs
@@ -13,7 +13,37 @@ namespace SportsStore.WebUI
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-            routes.MapRoute(
+            routes.MapRoute(null,
+                "",
+                new
+                {
+                    controller = "Product",
+                    action = "List",
+                    category = (string)null,
+                    page = 1
+                }
+            );
+            routes.MapRoute(null,
+               "Page{page}",
+               new { controller = "Product", action = "List", category = (string)null },
+               new { page = @"\d+" }
+           );
+
+            routes.MapRoute(null,
+                "{category}",
+                new { controller = "Product", action = "List", page = 1 }
+            );
+
+            routes.MapRoute(null,
+                "{category}/Page{page}",
+                new { controller = "Product", action = "List" },
+                new { page = @"\d+" }
+            );
+
+            routes.MapRoute(null, "{controller}/{action}");
+
+
+            /*routes.MapRoute(
                 name : null,
                 url:"page{page}",
                 defaults: new { Controller = "Product", action = "List" }
@@ -23,7 +53,7 @@ namespace SportsStore.WebUI
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Product", action = "List", id = UrlParameter.Optional }
-            );
+            );*/
         }
     }
 }
