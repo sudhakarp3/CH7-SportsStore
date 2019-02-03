d7448af29d0c282ccf9aca32311ba25416861a94
sudhakarp3 <sudha2u@gmail.com>
2019-02-03 20:28:10 +0400

    chapter 11 started page 277

---
+++ SportsStore.WebUI/Controllers/AdminController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SportsStore.Domain.Abstract;
+
+
+namespace SportsStore.WebUI.Controllers
+{
+    public class AdminController : Controller
+    {
+        private IProductRepository repository;
+        public AdminController(IProductRepository repo)
+        {
+            repository = repo;
+        }
+
+        // GET: Admin
+        public ActionResult Index()
+        {
+            return View(repository.Products);
+        }
+    }
+}
